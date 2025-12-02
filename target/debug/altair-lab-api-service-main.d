90651fe2475bad5f846ab2f28929c74c28fa0ecc
Napstablooky <121095746+Napstablooky@users.noreply.github.com>
2025-12-02 20:33:33 +0100

    [ADD] cargo

---
+++ target/debug/altair-lab-api-service-main.d
@@ -0,0 +1,1 @@
+/home/napoletanocyndelle/Bureau/altair-lab-api-service-main/target/debug/altair-lab-api-service-main: /home/napoletanocyndelle/Bureau/altair-lab-api-service-main/src/main.rs
---
+++ target/debug/deps/altair_lab_api_service_main-009726f4fbfbae5f.d
@@ -0,0 +1,5 @@
+/home/napoletanocyndelle/Bureau/altair-lab-api-service-main/target/debug/deps/altair_lab_api_service_main-009726f4fbfbae5f.d: src/main.rs
+
+/home/napoletanocyndelle/Bureau/altair-lab-api-service-main/target/debug/deps/altair_lab_api_service_main-009726f4fbfbae5f: src/main.rs
+
+src/main.rs:
